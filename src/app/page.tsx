daca195bba84508dc1532f7ca2480cd2c4e5b561
konveloper <konveloper@gmail.com>
2025-03-30 19:33:11 +0900

    feat: 메인 홈 UI, 로그인 리다이렉트, 공통 icon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,40 @@
 import Link from 'next/link';
+import { LogoSvg } from '@/components/ui';
 
-export default function MainPage() {
+export default function HomePage() {
   return (
-    <div>
-      <p>지금 일어나고 있는 일</p>
-      <p>지금 가입하세요.</p>
-      <Link href='/flow/signup'>계정 만들기</Link>
-      <p>이미 가입하셨나요?</p>
-      <Link href='/flow/login'>로그인</Link>
+    <div className='min-h-screen flex items-center justify-center'>
+      <div className='w-full flex gap-5'>
+        {/* 로고 및 소개 영역 */}
+        <div className='flex-1 flex items-center justify-center gap-2'>
+          <LogoSvg />
+          <p className='text-xl font-bold'>Hi, There!</p>
+        </div>
+        <div className='flex-1 flex flex-col items-start gap-5'>
+        {/* 회원가입 텍스트 영역 */}
+        <div className='flex flex-col gap-2'>
+          <p className='text-sm'>처음이신가요? 함께 시작해요.</p>
+          <Link
+            href='/flow/signup'
+            className="w-fit px-4 py-1 bg-black text-white rounded
+            hover:bg-blue-700 transition"
+          >
+            계정 만들기
+          </Link>
+        </div>
+        {/* 로그인 텍스트 영역 */}
+        <div className='flex flex-col gap-2'>
+          <p className='text-sm'>다시 오셨군요. 반가워요!</p>
+          <Link
+            href='/login' // 이후 '/flow/login' 리다이렉트
+            className="w-fit px-4 py-1 bg-black text-white rounded
+            hover:bg-blue-700 transition"
+          >
+            로그인
+          </Link>
+        </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/ui.tsx
@@ -0,0 +1,17 @@
+// atoms
+// atoms > icon
+import { LikeSvg, UnlikeSvg } from './atoms/icon/LikeSvg';
+import { LogoSvg } from './atoms/icon/LogoSvg';
+import { HomeSvg, ExploreSvg, MessageSvg, UserSvg } from './atoms/icon/SidebarIcons';
+
+export {
+  // atoms
+  // atoms > icon
+  LikeSvg,
+  UnlikeSvg,
+  LogoSvg,
+  HomeSvg,
+  ExploreSvg,
+  MessageSvg,
+  UserSvg
+}
